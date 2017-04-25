ff64eb356f344a39b49547bdcb2cc81f3189c34c
David L <david.landry@veldrane.de>
2017-04-25 12:48:38 +0200

    Use a server folder

--- server/main.js
+++ server/main.js
@@ -2,6 +2,7 @@ var express = require('express')
 var path = require('path')
 var compression = require('compression')
 
+var base_dir = path.resolve(__dirname, '..')
 var env = process.env['NODE_ENV']
 
 var app = express()
@@ -9,7 +10,7 @@ app.use(compression())
 
 if (env === 'development') {
   var webpack       = require('webpack');
-  var webpackConfig = require('./webpack.config');
+  var webpackConfig = require('../webpack.config');
   var compiler      = webpack(webpackConfig);
 
   app.use(require("webpack-dev-middleware")(compiler, {
@@ -19,13 +20,12 @@ if (env === 'development') {
   }));
 
   app.use(require("webpack-hot-middleware")(compiler));
-} else {
 }
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(base_dir, 'public')))
 
 app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+  res.sendFile(path.join(base_dir, 'public', 'index.html'))
 })
 
 var PORT = process.env.PORT || 8080
