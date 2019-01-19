e7a3d5080de2d7e03aabe16a527f73c06973786d
David L <david.landry@veldrane.de>
2019-01-19 11:45:19 +0100

    Add prettier

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,9 @@
 import React from 'react'
 import './App.scss'
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <div className='App'>
-        <h1>Hello world!</h1>
-      </div>)
-  }
-}
+const App = () =>
+  <div className='App'>
+    <h1>Hello world!</h1>
+  </div>
+
+export default App
